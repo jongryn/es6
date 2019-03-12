3c1ff1c300996e8043108c78ae688b6c219919e1
jgryn <jonathangryn@Jonathans-MacBook-Pro.local>
2019-03-12 15:23:11 -0700

    test

---
+++ script.js
@@ -0,0 +1,62 @@
+// Lecture: let and const
+
+
+// ES5
+/* 
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6); 
+*/
+
+// ES5
+function driversLicense5(passedTest) {
+
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+
+}
+
+
+
+driversLicense5(true);
+
+
+// ES6
+function driversLicense6(passedTest) {
+
+    // console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';
+    }
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicense6(true);
+
+
+
+
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
