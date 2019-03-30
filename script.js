844dfae2e6d9943a0c44c16cc63f71b006e6b8b9
jgryn <jonathangryn@Jonathans-MacBook-Pro.local>
2019-03-29 22:08:09 -0700

    test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 
 // ES5
+/*
 var name5 = 'Jane Smith';
 var age5 = 23;
 name5 = 'Jane Miller';
@@ -12,3 +13,61 @@ const name6 = 'Jane Smith';
 let age6 = 23;
 name6 = 'Jane Miller';
 console.log(name6);
+*/
+
+// ES5
+function driversLicense5(passedTest) {
+
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+
+        console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+
+    }
+}
+
+driversLicense5(true);
+
+// ES6
+function driversLicense6(passedTest) {
+
+    // console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';
+    }
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicense6(true);
+
+
+
+
+
+
+
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
+
+
+
+
+
+
+
+
+
+
+
